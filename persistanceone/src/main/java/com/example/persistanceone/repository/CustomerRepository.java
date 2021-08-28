0eed6edf010e1a7585bec2ecf14dc7a36fd8e2d0
henok keraga <henokkeraga@gmail.com>
2021-08-28 08:48:52 -0600

    spring-jdbc

--- persistanceone/src/main/java/com/example/persistanceone/repository/CustomerRepository.java
+++ persistanceone/src/main/java/com/example/persistanceone/repository/CustomerRepository.java
@@ -6,21 +6,28 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Array;
 import java.sql.JDBCType;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CustomerRepository {
 
     //datasource => where to execute the query
-    @Autowired
-    protected DataSource dataSource;
+    protected final DataSource firstDatasource;
 
     public JdbcTemplate jdbcTemplate;
 
+    public CustomerRepository(DataSource firstDatasource) {
+        this.firstDatasource = firstDatasource;
+    }
+
     public JdbcTemplate getJdbcTemplate() {
         if (null == jdbcTemplate) {
-            jdbcTemplate = new JdbcTemplate(dataSource);
+            jdbcTemplate = new JdbcTemplate(firstDatasource);
         }
         return jdbcTemplate;
     }
@@ -28,20 +35,20 @@ public class CustomerRepository {
 
     public String addCustomer(Customer customer) {
         String sql = String.format("insert into customer (id, name) values (%d,'%s')", customer.getId(), customer.getName());
-        System.out.printf("sql :::: "+ sql);
+        System.out.printf("sql :::: " + sql);
         getJdbcTemplate().execute(sql);
         return "done";
     }
-    public DataSource dataSource(){
-        return  dataSource;
-    }
+
 
     public String addListOfCustomer(List<Customer> customers) {
-
-        return null;
+        List<Object[]> cust = customers.stream().map(customer -> new Object[]{customer.getId(), customer.getName()}).collect(Collectors.toList());
+        getJdbcTemplate().batchUpdate("insert into customer (id, name) values (?,?)", cust);
+        return "done";
     }
 
     public List<Customer> getAllCustomer() {
+        getJdbcTemplate().execute("");
 
         return null;
     }
